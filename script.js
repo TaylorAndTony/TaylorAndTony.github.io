4ea9d068d4c54900f1cfa98db0879dc5b798b59c
TaylorAndTony <doyijian@163.com>
2022-04-17 20:18:01 +0800

    add basic contents

---
+++ script.js
@@ -0,0 +1,14 @@
+let githubPage = 'https://github.com/TaylorAndTony';
+let biliPage = 'https://space.bilibili.com/47256272';
+
+function toUrl(url) {
+    window.open(url)
+}
+
+function jumpMine() {
+    toUrl(githubPage)
+}
+
+function jumpBili() {
+    toUrl(biliPage)
+}
