9dadc8584292c132c82601f8fa1efd5fecefb6a0
TaylorAndTony <doyijian@163.com>
2022-04-18 10:14:08 +0800

    refactor using dynamic load

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function toUrl(url) {
  * @param {string} favicon 
  * @param {string} domain 
  */
-function webpageBoxFactory(website, favicon, domain, mountToID, largeBox=false) {
+function webpageBoxFactory(website, favicon, domain, mountToID, largeBox = false) {
     let wrapper = document.createElement('div');
     wrapper.className = 'box webpage-box';
 
@@ -59,55 +59,157 @@ function webpageBoxFactory(website, favicon, domain, mountToID, largeBox=false) 
     document.getElementById(mountToID).appendChild(wrapper);
 }
 
-function customFunctionBox(name, func, mountToID) {
-    let wrapper = document.createElement('div');
-    wrapper.className = 'box webpage-box';
-    wrapper.style.cssText = `
-    width: 150px;
-    height: 40px;
-    display: flex;
-    border-radius: 10px;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    cursor: pointer;
-    padding-left:20px;
-    `;
-
-    let desc = document.createElement('div');
-    desc.innerHTML = name;
-    desc.className = 'web-text';
-    wrapper.addEventListener('click', func)
-    wrapper.appendChild(desc);
-    document.getElementById(mountToID).appendChild(wrapper);
+function webpageBoxJsonFactory(jsonString) {
+    for (let dict of jsonString) {
+        webpageBoxFactory(
+            dict.website,
+            dict.favicon,
+            dict.name,
+            dict.id,
+            dict.large)
+    }
 }
 
 // init
 window.onload = function () {
     // repo
-    webpageBoxFactory('https://gitee.com/taylorandtony/avatar-with-border', 'https://gitee.com/favicon.ico', '头像加边框', 'repo-list', true);
-    webpageBoxFactory('https://gitee.com/taylorandtony/learn-rar-pls', 'https://gitee.com/favicon.ico', '简明解压教程', 'repo-list', true);
-    webpageBoxFactory('https://gitee.com/taylorandtony/play-linux', 'https://gitee.com/favicon.ico', '折腾 Linux', 'repo-list', true);
-    webpageBoxFactory('https://github.com/TaylorAndTony/AHK_Scripts', 'https://github.githubassets.com/favicon.ico', 'My AHK Script', 'repo-list', true);
-    webpageBoxFactory('https://github.com/TaylorAndTony/PhoneImageOCR', 'https://github.githubassets.com/favicon.ico', 'OCR your phone', 'repo-list', true);
-    // load `find me on these` boxes
-    webpageBoxFactory(biliPage, 'https://static.hdslb.com/images/favicon.ico', 'Bilibili', 'website-list');
-    webpageBoxFactory(giteePage, 'https://gitee.com/favicon.ico', 'Gitee', 'website-list');
-    webpageBoxFactory(githubPage, 'https://github.githubassets.com/favicon.ico', 'Github', 'website-list');
-    webpageBoxFactory(weiboPage, 'https://weibo.com/favicon.ico', 'Weibo', 'website-list');
-    webpageBoxFactory(acfunPage, 'https://www.acfun.cn/favicon.ico', 'Acfun', 'website-list');
-    webpageBoxFactory(happyPage, 'https://github.githubassets.com/favicon.ico', '乐道至简', 'website-list');
-    // load instersting box
-    webpageBoxFactory('https://neumorphism.io/#e0e0e0', 'https://neumorphism.io/favicon.ico', '2.5D 生成器', 'interesting-list', true);
-    webpageBoxFactory('https://byrut.org/', 'https://byrut.org/favicon.png', 'Russian torrents', 'interesting-list', true);
-    webpageBoxFactory('https://myacg.xyz/', 'https://mynovel.life/img/favicon.ico', 'MyACG 导航', 'interesting-list', true);
-    // anime
-    webpageBoxFactory('https://bbs.mihoyo.com/', 'https://bbs.mihoyo.com/favicon.ico', '米游社', 'anime-list');
-    webpageBoxFactory('http://www.imomoe.live/', 'https://github.githubassets.com/favicon.ico', '樱花动漫', 'anime-list');
-    webpageBoxFactory('https://xsear.ch/', 'https://xsear.ch/favicon.ico', 'Xsearch', 'anime-list');
-    webpageBoxFactory('https://gelbooru.com/', 'https://gelbooru.com/favicon.ico', 'Gelbooru', 'anime-list');
-    webpageBoxFactory('https://safebooru.org/index.php', 'https://safebooru.org/favicon.ico', 'safebooru', 'anime-list');
-    // webpageBoxFactory('https://idol.sankakucomplex.com/?tags=bondage', 'https://idol.sankakucomplex.com/favicon.ico', 'sankaku', 'anime-list');
-    // webpageBoxFactory('http://2021-03-09-xh953kvogkc.icu/index.php', 'http://2021-03-09-xh953kvogkc.icu/favicon.ico', '仓库', 'anime-list');
-
+    let config = [
+        {
+            website: 'https://gitee.com/taylorandtony/avatar-with-border',
+            favicon: 'https://gitee.com/favicon.ico',
+            name: '头像加边框',
+            id: 'repo-list',
+            large: true
+        },
+        {
+            website: 'https://gitee.com/taylorandtony/learn-rar-pls',
+            favicon: 'https://gitee.com/favicon.ico',
+            name: '简明解压教程',
+            id: 'repo-list',
+            large: true
+        },
+        {
+            website: 'https://gitee.com/taylorandtony/play-linux',
+            favicon: 'https://gitee.com/favicon.ico',
+            name: '折腾 Linux',
+            id: 'repo-list',
+            large: true
+        },
+        {
+            website: 'https://github.com/TaylorAndTony/AHK_Scripts',
+            favicon: 'https://github.githubassets.com/favicon.ico',
+            name: 'My AHK Script',
+            id: 'repo-list',
+            large: true
+        },
+        {
+            website: 'https://github.com/TaylorAndTony/PhoneImageOCR',
+            favicon: 'https://github.githubassets.com/favicon.ico',
+            name: 'OCR your phone',
+            id: 'repo-list',
+            large: true
+        },
+        // my pages
+        {
+            website: biliPage,
+            favicon: 'https://static.hdslb.com/images/favicon.ico',
+            name: 'Bilibili',
+            id: 'website-list',
+            large: false,
+        },
+        {
+            website: giteePage,
+            favicon: 'https://gitee.com/favicon.ico',
+            name: 'Gitee',
+            id: 'website-list',
+            large: false,
+        },
+        {
+            website: githubPage,
+            favicon: 'https://github.githubassets.com/favicon.ico',
+            name: 'Github',
+            id: 'website-list',
+            large: false,
+        },
+        {
+            website: weiboPage,
+            favicon: 'https://weibo.com/favicon.ico',
+            name: 'Weibo',
+            id: 'website-list',
+            large: false,
+        },
+        {
+            website: acfunPage,
+            favicon: 'https://www.acfun.cn/favicon.ico',
+            name: 'Acfun',
+            id: 'website-list',
+            large: false,
+        },
+        {
+            website: happyPage,
+            favicon: 'https://TaylorAndTony.github.io/happy-simple/favicon.ico',
+            name: 'Happy Simple',
+            id: 'website-list',
+            large: false,
+        },
+        // load instersting box
+        {
+            website: 'https://neumorphism.io/#e0e0e0',
+            favicon: 'https://neumorphism.io/favicon.ico',
+            name: '2.5D 生成器',
+            id: 'interesting-list',
+            large: true
+        },
+        {
+            website: 'https://byrut.org/',
+            favicon: 'https://byrut.org/favicon.png',
+            name: 'Russian torrents',
+            id: 'interesting-list',
+            large: true
+        },
+        {
+            website: 'https://myacg.xyz/',
+            favicon: 'https://mynovel.life/img/favicon.ico', 
+            name: 'MyACG 导航',
+            id: 'interesting-list',
+            large: true
+        },
+        // anime
+        {
+            website: 'https://bbs.mihoyo.com/', 
+            favicon: 'https://bbs.mihoyo.com/favicon.ico', 
+            name: '米游社', 
+            id: 'anime-list',
+            large: false
+        },
+        {
+            website: 'http://www.imomoe.live/',
+            favicon:  'https://github.githubassets.com/favicon.ico', 
+            name: '樱花动漫', 
+            id: 'anime-list',
+            large: false
+        },
+        {
+            website: 'https://xsear.ch/', 
+            favicon:  'https://xsear.ch/favicon.ico', 
+            name: 'Xsearch',
+            id: 'anime-list',
+            large: false
+        },
+        {
+            website: 'https://gelbooru.com/',
+            favicon:  'https://gelbooru.com/favicon.ico',
+            name:  'Gelbooru', 
+            id: 'anime-list',
+            large: false
+        },
+        {
+            website: 'https://safebooru.org/index.php', 
+            favicon:  'https://safebooru.org/favicon.ico', 
+            name:  'safebooru', 
+            id: 'anime-list',
+            large: false
+        },
+    ]
+    webpageBoxJsonFactory(config);
 }
